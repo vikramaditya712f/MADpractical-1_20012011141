338c5f4a4687d6f22a00cfe44dcd9c21beeff68a
Prince Verma <89698440+vikramaditya712f@users.noreply.github.com>
2022-08-22 21:49:34 +0530

    update

--- src/main/kotlin/practical-3.kt
+++ src/main/kotlin/practical-3.kt
@@ -1,21 +1,37 @@
 fun main()
 {
-    print("student Enrollment No. : ")
-    var x= readLine()!!.toInt()
+    print("Student enrollment no. : ")
+    val a = readLine()!!.toLong()
 
-//    print("Name : ")
-//    var y= readLine()!!.toInt()
-//
-//    print("Branch : ")
-//    var x= readLine()!!.toInt()
-//
-//    print("Class : ")
-//    var x= readLine()!!.toInt()
-//
-//    print("student Enrollment No. : ")
-//    var x= readLine()!!.toInt()
-//
-//    print("student Enrollment No. : ")
-//    var x= readLine()!!.toInt()
-//    println("x=$x ")
+    print("Student name : ")
+    val b = readLine()!!.toString()
+
+    print("Student branch : ")
+    val c = readLine()!!.toString()
+
+    print("Student class : ")
+    val d = readLine()!!.toString()
+
+    print("Student batch : ")
+    val e = readLine()!!.toString()
+
+    print("Student college name : ")
+    val f = readLine()!!.toString()
+
+    print("Student university name : ")
+    val g = readLine()!!.toString()
+
+    print("Student age : ")
+    val h = readLine()!!.toLong()
+
+    println("**************************8")
+    println("Student's data")
+    println("Enrollment no. : $a")
+    println("Name : $b")
+    println("Branch : $c")
+    println("Class : $d")
+    println("Batch : $e")
+    println("College name : $f")
+    println("University name : $g")
+    println("Age : $h")
 }

11f4f7a274b55efb2d9782509a5a8e16626190fa
Prince Verma <89698440+vikramaditya712f@users.noreply.github.com>
2022-08-22 18:12:45 +0530

    update

--- src/main/kotlin/practical-1.kt
+++ src/main/kotlin/practical-1.kt
@@ -1,10 +1,22 @@
 fun main()
 {
-    var x: Int=100
-    //val y=200
+    var a : Int = 2147483647
+    val b : Float = 22.2F
+    val c : Char = 'S'
+    val d : String = "LCA"
+    val e : Boolean = false
+    val f : Double = 22.2
+    val g : Long = 2147483648
+    val h : Short = -32768
+    val i : Byte = 127
 
-    var y: Double=100
-
-    println("x=$x")
-    println("y=$y")
+    println("Integer = $a")
+    println("Float = $b")
+    println("Character = $c")
+    println("String = $d")
+    println("Boolean = $e")
+    println("Double = $f")
+    println("Long = $g")
+    println("Short = $h")
+    println("Byte = $i")
 }
